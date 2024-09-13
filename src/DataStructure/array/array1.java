9f76b1ce247881b46b31d9f70d8844905364c664
AmreshSrivastava <srivastavaamresh4@gmail.com>
2024-09-13 23:43:53 +0530

    commited

--- src/DataStructure/array/array1.java
+++ src/DataStructure/array/array1.java
@@ -1,3 +1,7 @@
+package DataStructure.array;
+
 public class array1 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- src/DataStructure/hash/hash1.java
+++ src/DataStructure/hash/hash1.java
@@ -1,3 +1,5 @@
+package DataStructure.hash;
+
 public class hash1 {
     public static void main(String[]args){
         
