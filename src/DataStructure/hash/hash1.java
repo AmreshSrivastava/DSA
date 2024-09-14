0d8227469d94563a3cb3b98ff9e46e01aef8120b
AmreshSrivastava <srivastavaamresh4@gmail.com>
2024-09-14 16:26:04 +0530

    commited

--- src/DataStructure/hash/hash1.java
+++
@@ -1,7 +0,0 @@
-package DataStructure.hash;
-
-public class hash1 {
-    public static void main(String[]args){
-        
-    }
-}
--- src/DataStructure/linkedlist/ll1.java
+++ src/DataStructure/linkedlist/ll1.java
@@ -2,14 +2,17 @@ package DataStructure.linkedlist;
 
 import java.util.LinkedList;
 
-class ll {
-    public static void main(String[]args){
+public class ll1 {
+    public static void main(String[] args) {
         LinkedList<String> l = new LinkedList<String>();
         l.add("1");
         l.add("2");
         l.add("3");
         l.add("4");
 
-        System.out.println(l);
+        for(int i=l.size(); i<=0; i--){
+         System.out.println(l.get(i));
+        }
+        System.out.println();
     }
 }
